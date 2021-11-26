b4cb44982ec968dd3ebefc1e47f2e753ef78f7b1
dineshk876 <dinesh876@gmail.com>
2021-11-26 19:07:19 +0530

    need to add

---
+++ src/test/java/org/gittest/Testcase.java
@@ -0,0 +1,17 @@
+package org.gittest;
+
+import org.base.Base;
+import org.testng.annotations.Test;
+
+public class Testcase extends Base{
+	@Test
+private void testcase1() {
+		
+	System.out.println("java");
+	System.out.println("selenium");
+	System.out.println("cucumber");
+	
+	
+
+}
+}
