2fdb75db47870928503ed5f5b77a84d8f09c0189
dineshk876 <dinesh876@gmail.com>
2021-11-26 19:33:35 +0530

    update

--- src/test/java/org/gittest/Testcase.java
+++ src/test/java/org/gittest/Testcase.java
@@ -3,7 +3,10 @@ package org.gittest;
 import org.junit.Test;
 
 public class Testcase {
-@Test
+public static void main(String[] args) {
+	Testcase t=new Testcase();
+	t.testcase1();
+}
 private void testcase1() {
 	System.out.println("java");
 	System.out.println("selenium");
