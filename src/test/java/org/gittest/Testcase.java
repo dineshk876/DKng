fb708bffffe10365d05f9ffe158ec2a204e6bd2c
dineshk876 <dinesh876@gmail.com>
2021-11-27 17:17:24 +0530

    case

--- src/test/java/org/gittest/Testcase.java
+++ src/test/java/org/gittest/Testcase.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	Testcase t=new Testcase();
 	t.testcase1();
 	t.testcase2();
-	t.testcase3();
+	t.testcase33();
 	}
 private void testcase1() {
 		
@@ -23,10 +23,10 @@ private void testcase2() {
 	System.out.println("cucumber1");
 	
 }
-private void testcase3() {
-	System.out.println("java2");
-	System.out.println("selenium2");
-	System.out.println("cucumber3");
+private void testcase33() {
+	System.out.println("java222");
+	System.out.println("selenium222");
+	System.out.println("cucumber322");
 	
 }
 }
